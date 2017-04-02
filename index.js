233dddff75c9d4ef57b222a93c155585b0ab3fd4
thastreet <mat_1794@hotmail.com>
2017-04-02 19:57:37 -0400

    Parsing basic player info

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var app = express();
-var request = require('request');
+const express = require('express');
+const app = express();
+const request = require('request');
+const cheerio = require('cheerio');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -8,14 +9,35 @@ app.get('/', function (req, res) {
     const battletag = req.query.battletag;
 
     if (battletag) {
-
-
-
-        res.sendStatus(200);
+        request('https://playoverwatch.com/en-us/career/pc/us/' + battletag, function (error, response, body) {
+            if (response && response.statusCode) {
+                if (response.statusCode === 200) {
+                    parseBody(res, body);
+                } else {
+                    res.sendStatus(response.statusCode);
+                }
+            }
+        });
     } else {
         res.sendStatus(400);
     }
 });
+
+function parseBody(res, body) {
+    const $ = cheerio.load(body);
+    const result = {};
+
+    result.playerName = $('.header-masthead').text();
+    result.level = $('.player-level', '.masthead-player').children().first().text();
+    result.levelFrameImageUrl = getContentBetweenParenthesis($('.player-level', '.masthead-player').css('background-image'));
+    result.rankImageUrl = getContentBetweenParenthesis($('.player-rank', '.masthead-player').css('background-image'));
+
+    res.json(result);
+}
+
+function getContentBetweenParenthesis(input) {
+    return input.substring(input.indexOf('(') + 1, input.indexOf(')'));
+}
 
 app.listen(app.get('port'), function () {
     console.log('Listening on port ' + app.get('port'));
