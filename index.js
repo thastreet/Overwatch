04ca250daa7a5c8bb8fb7a43077fe7df6d549c87
thastreet <mat_1794@hotmail.com>
2017-04-02 12:59:08 -0400

    Set up the port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function (req, res) {
     const battletag = req.query.battletag;
 
@@ -15,6 +17,6 @@ app.get('/', function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000!');
+app.listen(app.get('port'), function () {
+    console.log('Listening on port ' + app.get('port'));
 });
